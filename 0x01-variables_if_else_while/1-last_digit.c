03c807b273f5d897b50b4bf1b51f4ea30d0cf842
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-07-14 15:13:33 -0700

    the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Program assigning a random number to the variable n.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) < 5)
+	{
+		printf("%i is greater than 5\n", n);
+	}
+	else if ((n % 10) == 0)
+	{
+		printf("%i is 0\n", n);
+	}
+	else
+	{
+		printf("%i is less than 6 and not 0\n", n);
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 # Descriptions
 
 0-positive_or_negative.c - A program that assign a random number to the variable n each time it is executed.
+1-last_digit.c - A program that assigns a random number to the variable n each time it is executed.
