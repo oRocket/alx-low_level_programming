b19824b3500a81d0e2f9ba259f72d10aa1ca4cee
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-07-16 13:21:34 -0700

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,20 @@ int main(void)
 
 	for (i = 10; i <= 19; i++)
 	{
-	for (j = 10; j <= 19; j++)
-	{
-		if ((j % 10) > (i % 10))
+		for (j = 10; j <= 19; j++)
 		{
-		putchar((i % 10) + '0');
-		putchar((j % 10) + '0');
-		if (i != 18 || j != 19)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+			if ((j % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
-	}
 	return (0);
 }
