bec983bb6aeb807bfb8bb2248bb24864ae4671a2
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-07-18 13:59:03 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,24 +2,39 @@
 #include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from n to 98
- *
  * @n: natural numbers starting point
  * Return: Always 0 (Success)
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	if (n <= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+			printf("%d", n);
+			printf("\n");
+			}
+			else
+			{
+			printf("%d, ", n);
+			}
+		}
 	}
-	while (n > 98)
+	else
 	{
-		printf("%d, ", n);
-		n--;
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+			printf("%d", n);
+			printf("\n");
+			}
+			else
+			{
+			printf("%d, ", n);
+			}
+		}
 	}
-	if (n == 98)
-		printf("%d, ", n);
-
-	printf("\n");
 }
