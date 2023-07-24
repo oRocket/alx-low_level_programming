21ca90651b706f0f8371e4f0f04e1d94f32734f9
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-07-24 16:41:02 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,17 +9,11 @@ void puts2(char *str)
 	int c; /*count (c) variable definition*/
 
 	c = 0;
-	while (c >= 0)
+	while (str[c] != '\0')
 	{
-		if (str[c] == '\n')
-		{
-			_putchar('\n');
-			break;
-		}
 		if (c % 2 == 0)
-		{
 			_putchar(str[c]);
-		}
 		c++;
 	}
+	_putchar('\n');
 }
