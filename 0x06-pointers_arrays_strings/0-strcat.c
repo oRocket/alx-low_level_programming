e6ae8a3c35e8aecfb672405bd5d34d717363515d
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-07-27 03:45:24 -0700

    starting

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <string.h>
-/*
+/**
  * *_strcat - concatenates two strings
  * @dest: string 1
  * @src: string 2
  * Return: dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
