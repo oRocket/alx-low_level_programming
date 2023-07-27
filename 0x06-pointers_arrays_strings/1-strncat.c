bed44ab4efdd039c291bd80da4144af3478652cf
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-07-27 09:03:54 -0700

    concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
 
-	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	while (j < n)
 	{
-		dest[i] = src[j];
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
 	}
-	dest[i] = '\0';
-
 	return (dest);
 }
