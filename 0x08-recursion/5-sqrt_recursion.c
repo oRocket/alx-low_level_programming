243886bd3083ae27e1fa1378914ce6cf371d0057
root <aotwum.at@outlook.com>
2023-08-02 17:23:49 +0000

    dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,36 @@
 #include "main.h"
 /**
+ * assist - assist function for _sqrt_recursion
+ * @i: Number to evaluate
+ * @j: Number to square
+ * Return: square root
+ */
+int assist(int i, int j)
+{
+	if (j * j > i)
+	{
+		return (-1);
+	}
+	else if (j * j == i)
+	{
+		return (j);
+	}
+	else
+	{
+		return (assist(i, j + 1));
+	}
+	return (1);
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number for argument
+ * @n: number to be evaluated
  * Return: natural square root of n
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	if (n == 0)
 	{
-		return (-1);
+		return (0);
 	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	return (_sqrt_assist(n, 1));
-}
-/**
- * _sqrt_assist - aids function for _sqrt_recursion
- * @n: number for argument
- * @a: integer to square
- * Return: naturak square root of n
- */
-int _sqrt_assist(int n, int a)
-{
-	if (a * a == n)
-	{
-		return (a);
-	}
-	if (a * a > n)
-	{
-		return (-1);
-	}
-	return (_sqrt_assist(n, a + 1));
+	return (assist(n, 1));
 }
