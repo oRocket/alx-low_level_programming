31bdd00685d265de85f9520f764cd0d35b257adf
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-08-07 09:37:30 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,19 +11,19 @@
 char *_strdup(char *str)
 {
 	char *new_str;
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	new_str = malloc(strlen(str) + 1);
-	
+
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	strcpy(new_str, str);
 	return (new_str);
 }
