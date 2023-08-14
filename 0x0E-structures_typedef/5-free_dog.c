a8d602a153b113b03090e825aa1627ba7a95373e
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-08-14 14:40:05 -0700

    How many legs does a dog have

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dog
+ * @d: instance tl free dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+	{
+		free(d->name);
+	}
+	if (d->owner)
+	{
+		free(d->owner);
+	}
+	free(d);
+}
