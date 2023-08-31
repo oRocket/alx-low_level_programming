88f6d5d5a5759ec4d9474f92406c88f44a2273b8
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-08-31 11:00:46 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
- * print_binary - prints the binary representation of a number.
- * @n: the number to print in library.
- * Return: Always 0 (Success)
+ * print_binary - Prints the binary representation of a number.
+ * @n: Number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-	{
-		print_binary(n >> 1); /* Right-shift n by 1 */
-	}
-	/* print the least significant bit of n */
-	printf("%lu", n & 1);
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
 }
