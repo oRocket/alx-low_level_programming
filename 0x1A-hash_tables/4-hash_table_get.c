296377f0205f2e44810970980e1f0c96efe255ed
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-11-02 13:02:27 -0700

    >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves a value associated 
+ * hash_table_get - Retrieves a value associated
  * with a key in a hash table.
  * @ht: The hash table to search in.
  * @key: The key to search for.
- * Return: The value associated with the key, 
+ * Return: The value associated with the key,
  * or NULL if the key couldn't be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
