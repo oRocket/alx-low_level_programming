cbbf2ca1db6907e9b47aad9a7b8e7bb78141bbfe
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-20 14:54:02 -0800

    make -f 0-Makefile

---
+++ 0x1C-makefiles/list.h
@@ -0,0 +1,9 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_alx(void);
+
+#endif /* LIST.H */
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "list.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	print_alx();
+	return (EXIT_SUCCESS);
+}
